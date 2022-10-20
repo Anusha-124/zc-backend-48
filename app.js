3aa820beb3155b8515d4de4c4382d1049fec0065
Anusha-124 <anusha.kokkonda221@gmail.com>
2022-10-20 13:56:24 +0530

    API Project

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const APIRouter = require("./Routes/APIRouter");
 
 
 const PORT = process.env.PORT || 5003;
-const URI = 'mongodb+srv://anusha:9966621393@edurekaa.txy4g5e.mongodb.net/bacth48edureka?retryWrites=true&w=majority'
+const URI = 'mongodb+srv://anusha:anusha@edurekaa.txy4g5e.mongodb.net/batch48edureka?retryWrites=true&w=majority';
 
 app.use(express.json()); // convert string JSON data to pure json data
 app.use(express.urlencoded({ extended: false })); // normal post data  to json data
@@ -23,7 +23,7 @@ mongoose.connect(URI).then(() => {
     app.listen(PORT, () => {
         console.log('db connected successfully!!!!....')
         console.log("zomato  api is running on port :", PORT);
-        console.log("hello...fffff")
+        console.log("hello...")
 
     });
 
